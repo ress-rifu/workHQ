62bbe1ec031c007975cfb054e436d5505ca86c16
khorshedsadhin <khorshedsadhin81@gmail.com>
2025-11-10 21:11:44 +0600

    backend problem fixed#01

--- Back/src/utils/prisma.ts
+++ Back/src/utils/prisma.ts
@@ -1,9 +1,15 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
 
 const prisma = new PrismaClient({
-  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+	log:
+		process.env.NODE_ENV === "development"
+			? ["query", "error", "warn"]
+			: ["error"],
 });
 
 export { prisma };
 export default prisma;
-
